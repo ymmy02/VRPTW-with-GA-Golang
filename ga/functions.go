8aeb4299bef5b5bbb8357ce5b4718edb08aa7737
Kohei Yamamoto <kh2yam1@gmial.com>
2017-12-06 20:29:23 -0500

    [chage] Add some functions

--- ga/functions.go
+++ ga/functions.go
@@ -1,7 +1,9 @@
 package ga
 
 import (
+	"../node"
 	"fmt"
+	"math"
 	"os"
 )
 
@@ -14,6 +16,68 @@ const (
 //*********//
 // Private //
 //*********//
+func calcDistanceBetweenNodes(x1, y1, x2, y2 float64) float64 {
+	return math.Sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2))
+}
+
+func calcOneVehicleDistance(nodes *node.NodeList, route []int) float64 {
+	depot := nodes.Depot()
+	x_depot, y_depot := depot.Position()
+	var totalDistance float64 = 0.0
+
+	x, y := nodes.Position(route[0])
+	totalDistance += calcDistanceBetweenNodes(x_depot, y_depot, x, y)
+	for i := 0; i < len(route)-1; i++ {
+		x1, y1 := nodes.Position(route[i])
+		x2, y2 := nodes.Position(route[i+1])
+		totalDistance += calcDistanceBetweenNodes(x1, y1, x2, y2)
+	}
+	x, y = nodes.Position(route[len(route)-1])
+	totalDistance += calcDistanceBetweenNodes(x, y, x_depot, y_depot)
+
+	return totalDistance
+}
+
+func clacDistance(nodes, chromosome [][]int) float64 {
+	var totalDistance float64 = 0.0
+	for _, route := range chromosome {
+		totalDistance += calcOneVehicleDistance(nodes, route)
+	}
+	return totalDistance
+}
+
+func shuffle(data []int) {
+	n := len(data)
+	for i := n - 1; i >= 0; i-- {
+		j := rand.Intn(i + 1)
+		data[i], data[j] = data[j], data[i]
+	}
+}
+
+func shapeFlatToVehicles(nodes *node.NodeList, flattench []int) [][]int {
+	chromosome := make([][]int)
+	size := len(flattench)
+	var cut1, cut2 int = 0, 0
+	shuffle(flattench)
+	for cut1 < size {
+		breakFlag := false
+		for cut2 = cut1; cut2 < size+1; cut2++ {
+			route := flattench[cut1:cut2]
+			if !nodes.is_feasible(route) {
+				cut1 = cut2 - 1
+				route = route[:len(route)-1]
+				breakFlag = true
+				break
+			}
+		}
+		if breakFlag {
+			cut1 = cut2
+		}
+		chromosome = append(chromosome, route)
+	}
+	return chromosome
+}
+
 func copyIndividual(indv Individual) *Individual {
 	var newch [][]int
 	copy(newch, indv.Chromosome)
@@ -24,7 +88,6 @@ func copyIndividual(indv Individual) *Individual {
 		Distance:   distance,
 		Fitness:    fitness,
 	}
-
 	return newIndv
 }
 
@@ -67,6 +130,15 @@ func makeParetoRankingList(indvList []*Individual) [][]*Individual {
 //********//
 // Public //
 //********//
+func CreateIndividualList(population int, nodes *node.NodeList) []*Individual {
+	indvList := make([]*Individual, 0, population)
+	for i := 0; i < population; i++ {
+		indv = CreateIndividualList(nodes)
+		indvList = append(indvList, indv)
+	}
+	return indvList
+}
+
 func MakeCurrentRankingList(currentRankCandidates []*Individual) ([]*Individual, []*Individual) {
 	dominatedList := make([]*Individual, 0, len(currentRankCandidates))
 	nondominatedList := make([]*Individual, 0, len(currentRankCandidates))
